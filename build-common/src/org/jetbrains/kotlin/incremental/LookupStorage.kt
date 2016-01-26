b51418871eecd1fbfef934cba6c022b06e38e28c
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2016-01-26 19:04:11 +0300

    ~~~ use interner in LookupTrackerImpl

--- build-common/src/org/jetbrains/kotlin/incremental/LookupStorage.kt
+++ build-common/src/org/jetbrains/kotlin/incremental/LookupStorage.kt
@@ -17,6 +17,7 @@
 package org.jetbrains.kotlin.incremental
 
 import com.intellij.util.containers.MultiMap
+import com.intellij.util.containers.StringInterner
 import org.jetbrains.annotations.TestOnly
 import org.jetbrains.kotlin.incremental.components.LookupTracker
 import org.jetbrains.kotlin.incremental.components.Position
@@ -183,13 +184,18 @@ open class LookupStorage(private val targetDataDir: File) : BasicMapsOwner() {
 
 class LookupTrackerImpl(private val delegate: LookupTracker) : LookupTracker {
     val lookups = MultiMap<LookupSymbol, String>()
+    val pathInterner = StringInterner()
+    val interner = StringInterner()
 
     override val requiresPosition: Boolean
         get() = delegate.requiresPosition
 
     override fun record(filePath: String, position: Position, scopeFqName: String, scopeKind: ScopeKind, name: String) {
-        lookups.putValue(LookupSymbol(name, scopeFqName), filePath)
-        delegate.record(filePath, position, scopeFqName, scopeKind, name)
+        val internedScopeFqName = interner.intern(scopeFqName)
+        val internedName = interner.intern(name)
+        val internedFilePath = pathInterner.intern(filePath)
+        lookups.putValue(LookupSymbol(internedName, internedScopeFqName), internedFilePath)
+        delegate.record(internedFilePath, position, internedScopeFqName, scopeKind, internedName)
     }
 }
 
